17ae873a379b370d57182d0abeedeba413ed7bfd
Jdemule <ouldrabia.jamal@gmail.com>
2023-04-17 11:09:47 +0200

    Couleur

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+console.log(chalk.blue('Hello Marcos'));
+console.log(chalk.red('Hello Yannick'));
+console.log(chalk.green('Hello Rachid'));
+console.log(chalk.yellow('Hello Christophe'));
